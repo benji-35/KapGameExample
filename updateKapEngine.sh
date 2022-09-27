2884da45990b6e54549a9132446f0f66b536d705
benji-35 <benjamin.delvert@epitech.eu>
2022-09-27 12:56:22 +0200

    ADD : engine update file

--- updateKapEngine.sh
+++ updateKapEngine.sh
@@ -3,4 +3,4 @@ rm -rf Library/KapEngine/
 rm -rf .git/modules/Library/KapEngine/
 git submodule add git@github.com:benji-35/KapEngine.git Library/KapEngine
 clear
-rm -rf build/ && git submodule update --init --recursive && mkdir build && cd build && cmake -G Ninja .. && ninja && clear && ./RType
+rm -rf build/ && git submodule update --init --recursive && mkdir build && cd build && cmake -G Ninja .. && ninja && clear && ./GameExample
