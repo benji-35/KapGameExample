c188969cede23fc05f4c1f8959c408ccd989a27d
benji-35 <benjamin.delvert@epitech.eu>
2022-10-28 15:34:57 +0200

    ADD : flags

--- Library/library.cmake
+++ Library/library.cmake
@@ -1,4 +1,9 @@
 # KapEngine
+
+option(KAPENGINE_THREAD "" ${GAME_THREAD})
+option(KAPENGINE_BETA "" ${GAME_BETA})
+option(KAPENGINE_DEBUG "" ${GAME_DEBUG})
+option(KAPENGINE_EDITOR "" OFF)
 add_subdirectory(Library/KapEngine)
 target_link_libraries(${EXE_NAME} KapEngine)
 include(Library/KapEngine/CMake/engine_include.cmake)
